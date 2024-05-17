c65ac8ed537c44cd2785d520ecea9631a3abc8f9
Vegard B. Bugge <vegabug@live.no>
2024-05-18 01:31:50 +0200

    Define Vite config for demo app; add directory to put tests in; define TS config

---
+++ demo/vite.config.ts
@@ -0,0 +1,15 @@
+import svgr from 'vite-plugin-svgr'
+
+import react from '@vitejs/plugin-react'
+import path from 'path'
+
+// https://vitejs.dev/config/
+export default {
+	plugins: [react(), svgr()],
+	resolve: {
+		alias: {
+			'@package': path.resolve('../src')
+		}
+	},
+	root: '.'
+}

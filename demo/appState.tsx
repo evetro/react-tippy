a44fae1a439dba3eea9efeafc85d279957f067bb
Vegard B. Bugge <vegabug@live.no>
2024-05-19 23:25:37 +0200

    Refactor: correct file ending

--- demo/appState.tsx
+++ demo/appState.tsx
@@ -1,12 +1,10 @@
-import { createContext, useContext, Provider, ReactNode, useMemo, useReducer } from 'react'
+import React, { createContext, useContext, ReactNode, useMemo, useReducer } from 'react'
 
 import { StateUpdater, NextState, StateDispatcher, AppState, AppStore } from './types'
 
 const AppContext = createContext<AppStore | undefined>(undefined)
 
 export const useAppStore = () => useContext(AppContext)
-
-const { Provider } = AppContext
 
 export function AppProvider({ children }: { children: ReactNode }) {
 	const [state, setState] = useReducer((previous: AppState, update: NextState<AppState>) => {
@@ -17,9 +15,12 @@ export function AppProvider({ children }: { children: ReactNode }) {
 				return Object.assign(next, fn(previous))
 			}
 			return Object.assign(next, update)
-	}, { content: 'CONTENT' })
-
+	}, { content: 'CONTENT', tooltipContent: 'tooltipContent', open: false, disabled: false } as AppState)
 	const value = useMemo<[AppState, StateDispatcher<AppState>]>(() => [state, setState], [state])
 
-	return <Provider value={value}>{children}</Provider>
+	return (
+		<AppContext.Provider value={value}>
+			<>{children}</>
+		</AppContext.Provider>
+	)
 }

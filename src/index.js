7276367d988db0ee99320162dcd34dd995a4f624
Vegard Bjerkli Bugge <vegabug@live.no>
2024-06-09 23:28:46 +0200

    Put type export in main entrypoint

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import Tooltip from './component';
-import withTooltip from './hoc';
-
-export {
-  Tooltip,
-  withTooltip,
-}
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import Tooltip from './component'
+import withTooltip from './hoc'
+
+export * from './types'
+
+export { Tooltip, withTooltip }

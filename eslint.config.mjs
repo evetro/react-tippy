ae7ba7e727a00d4ba582e698cdcb3f41a24a8dd4
Vegard B. Bugge <vegabug@live.no>
2024-08-03 19:34:47 +0200

    delete deprecated configs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,37 +6,47 @@ import react from 'eslint-plugin-react'
 import tsLintPlugin from '@typescript-eslint/eslint-plugin'
 import tsLintParser from '@typescript-eslint/parser'
 
+// util function which denotes file descriptors by directory name
 const paths = dir => [`./${dir}/**.*.ts`, `./${dir}/**.*.tsx`]
 
 const { browser, es2015, node, serviceWorker } = glob
-const { configs: { recommended } } = js
+
+const { configs: { recommended: { rules } } } = js
+
 const {
   configs: {
-    recommended: jsxRecommended,
-	['jsx-runtime']: jsxRuntime
+    recommended: { rules: jsxRules },
+    'jsx-runtime': { rules: jsxRuntimeRules }
   }
 } = react
+
+const {
+  configs: {
+    recommended: { rules: tsRules },
+    'eslint-recommended': { rules: tsLintRules }
+  }
+} = tsLintPlugin
 
 const plugins = { react, '@typescript-eslint': tsLintPlugin, tsLintPlugin }
 
 export default [
   {
-    files: [...paths('demo'), ...paths('src')],
+    files: [...paths('demo'), ...paths('src'), ...paths('tests')],
     rules: {
-      ...recommended.rules,
-      ...jsxRecommended.rules,
-      ...jsxRuntime.rules,
-      ...tsLintPlugin.configs['eslint-recommended'].rules,
-      ...tsLintPlugin.configs['recommended'].rules,
+      ...rules,
+      ...jsxRules,
+      ...jsxRuntimeRules,
+      ...tsRules,
+      ...tsLintRules,
       ...pluginSecurity.rules,
       ...eslintConfigPrettier.rules,
       'security/detect-object-injection': 0,
       'no-use-before-define': [0],
-      'no-console': 2, // 0 for demo
+      'no-console': 2,
       'import/newline-after-import': [2, { count: 1 }]
-    },
+    }
     plugins,
-	jsxPragma: null,
+    jsxPragma: null,
     languageOptions: {
       parser: tsLintParser,
       sourceType: 'module',

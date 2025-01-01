8977c088f4a5c47ca0a4200e148af9f8ceec47d3
Vegard B. Bugge <vegabug@live.no>
2025-01-01 14:35:59 +0100

    Implement util function

--- src/utils.ts
+++ src/utils.ts
@@ -11,3 +11,24 @@ export function getAttributeName(selector: string): string {
 export function getCorePlacement(placement: string): string {
 	return placement.replace(/-.+/, '')
 }
+
+/**
+* Returns an array of elements based on the selector input
+*/
+export function getArrayOfElements(
+	selector?: string | Element | Element[] | NodeListOf<Element>
+): Element[] {
+	if (selector instanceof Element) {
+		return [selector]
+	}
+
+	if (selector instanceof NodeList) {
+		return Array.from(selector) as Element[]
+	}
+
+	if (Array.isArray(selector)) {
+		return selector
+	}
+  
+	return [].slice.call(document.querySelectorAll(selector))
+}
--- test/utils.test.ts
+++ test/utils.test.ts
@@ -1,5 +1,6 @@
-import { getAttributeName, getCorePlacement } from '../src/utils'
+import { getAttributeName, getCorePlacement, getArrayOfElements } from '../src/utils'
 import { POPPER, TOOLTIP, CONTENT, CONTROLLER, TOOLTIPPED_EL, ARROW, CIRCLE } from '../src/selectors'
+import createNewElement from './createNewElement'
 
 describe('getAttributeName module', () => {
 	it('remains prefix dot from class IDs', () => {
@@ -39,5 +40,50 @@ describe('getCorePlacement module', () => {
 		expect(getCorePlacement('top-end')).toBe('top')
 		expect(getCorePlacement('right-end')).toBe('right')
 		expect(getCorePlacement('left-end')).toBe('left')
+	})
+})
+
+describe('getArrayOfElements', () => {
+	it('returns an empty array with no arguments', () => {
+		expect(Array.isArray(getArrayOfElements())).toBe(true)
+	})
+
+	it('returns the same array if given an array', () => {
+		const arr = []
+		expect(getArrayOfElements(arr)).toBe(arr)
+	})
+
+	it('returns an array of elements when given a valid selector string', () => {
+		const fn = () => createNewElement()
+		const isElement = (value) => (value instanceof Element)
+		const list = [...Array(10)]
+		list.forEach(fn)
+		const allAreElements = getArrayOfElements('__tippy').every(isElement)
+		expect(allAreElements).toBe(true)
+	})
+
+	it('returns an empty array when given an invalid selector string', () => {
+		const arr = getArrayOfElements('😎')
+
+		expect(Array.isArray(arr)).toBe(true)
+		expect(arr.length).toBe(0)
+	})
+
+	it('returns an array of length 1 if the value is a DOM element', () => {
+		const ref = createNewElement()
+		const arr = getArrayOfElements(ref)
+
+		expect(arr[0]).toBe(ref)
+		expect(arr.length).toBe(1)
+	})
+
+	it('returns an array if given a NodeList', () => {
+		const ref = createNewElement()
+		const arr = getArrayOfElements(
+			document.querySelectorAll('.__tippy')
+		)
+
+		expect(arr[0]).toBe(ref)
+		expect(Array.isArray(arr)).toBe(true)
 	})
 })
